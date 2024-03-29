20e4722b953022f91eba0496eadfe40a08e9b2cc
Christina Kozanian <christinakozanian@gmail.com>
2024-03-29 15:57:21 -0700

    lab complete

--- server/migrations/versions/94931d70704e_.py
+++ server/migrations/versions/94931d70704e_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: faa482c1e292
+Revision ID: 94931d70704e
 Revises: 
-Create Date: 2023-11-04 17:22:44.672769
+Create Date: 2024-03-29 15:29:36.255506
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'faa482c1e292'
+revision = '94931d70704e'
 down_revision = None
 branch_labels = None
 depends_on = None
--- server/models.py
+++ server/models.py
@@ -13,10 +13,10 @@ class Author(db.Model):
 
     @validates('name')
     def validate_name(self, key, name):
-        if name is None or name == '':
+        if not name:
             raise ValueError('Author must have a name')
         existing_author = Author.query.filter(Author.name == name).first()
-        if existing_author:
+        if existing_author is not None:
             raise ValueError('No two authors have the same name.')
         return name
         
@@ -40,34 +40,41 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    @validates('content')
-    def validate_content(self, key, content):
-        if len(content) < 250:
-            raise ValueError("Post content must be at least 250 characters long.")
-        return content
-    
-    @validates('summary')
-    def validate_summary(self, key, summary):
-        if len(summary) > 250:
-            raise ValueError("Summary must be less than 250 characters long.")
-        return summary
+
+    @validates('title')
+    def validate_title(self, key, title):
+        phrases = ["Won't Believe", "Secret", "Top", "Guess"]
+        if title is None or title == '':
+            raise ValueError('Post must have a title')
+        titles = []
+        for phrase in phrases:
+            if phrase in title:
+                titles.append(phrase)
+        if not titles:
+            raise ValueError("Title must contain clickbait")
+        return title
+
+    @validates('content', 'summary')
+    def validate_length(self, key, string):
+        if key == 'content':
+            if len(string) < 250:
+                raise ValueError("Post content must be at least 250 characters long.")
+        if key == 'summary':
+            if len(string) > 250:
+                raise ValueError("Summary must be less than 250 characters long.")
+        return string
+        
+    # @validates('summary')
+    # def validate_summary(self, key, summary):
+    #     if len(summary) > 250:
+    #         raise ValueError("Summary must be less than 250 characters long.")
+    #     return summary
     
     @validates('category')
     def validate_category(self, key, category):
         if category not in ["Fiction", "Non-Fiction"]:
             raise ValueError("Category must be either Fiction or Non-Fiction")
         return category
-    
-    @validates('title')
-    def validate_title(self, key, title):
-        phrases = ["Won't Believe", "Secret", "Top", "Guess"]
-        if title is None or title == '':
-            raise ValueError('Post must have a title')
-        for phrase in phrases:
-            if phrase not in title:
-                raise ValueError("Title must contain clickbait")
-        return title
-    
-
+   
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
